0babb592aed2b03193cb497d6983e3a2e8a81b02
Stuart Ellis <stuart@stuartellis.name>
2021-02-24 23:55:38 +0000

    Fill out initial version

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # nuklear
-Runner for aws-nuke
+
+Runner for [aws-nuke](https://github.com/rebuy-de/aws-nuke).
+
+## WARNING: VPC Resources
+
+By default, it will destroy default VPC network resources:
+
+https://github.com/rebuy-de/aws-nuke/issues/555
+
+To avoid this, exclude VPC resources from destruction:
+
+- EC2VPC
+- EC2Subnet
+- EC2InternetGatewayAttachment
+- EC2NetworkACL
+- EC2RouteTable
+- EC2DHCPOption
+- EC2InternetGateway
+
+## WARNING: Handling S3 Buckets
+
+By default, aws-nuke checks and logs every individual object in S3 buckets.
+
+To avoid this, nuke only the S3Bucket resources. This will clean up all the objects within the buckets, without logging all of the individual objects.
---
+++ do.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export ARCH=darwin-amd64
+export AWS_NUKE_VERSION=2.15.0.rc.3
+export AWS_NUKE_FILE=aws-nuke-v$AWS_NUKE_VERSION-$ARCH
+export AWS_NUKE_URL=https://github.com/rebuy-de/aws-nuke/releases/download/v2.15.0-rc.3/$AWS_NUKE_FILE.tar.gz
+export AWS_NUKE_EXE=aws-nuke
+export AWS_NUKE_CONFIG=config/nuke-config-limited.yml
+export TIMESTAMP=$(date +%Y-%m-%dT%H:%M%z)
+
+case $1 in
+  info)
+    ./$AWS_NUKE_EXE version
+  ;;
+  clean)
+   [ -f "$AWS_NUKE_EXE" ] && rm $AWS_NUKE_EXE
+   [ -d "log" ] && rm -r log 
+  ;;
+  dryrun)
+    ./$AWS_NUKE_EXE --config $AWS_NUKE_CONFIG
+  ;;
+  nuke)
+    ./$AWS_NUKE_EXE --config $AWS_NUKE_CONFIG --no-dry-run
+  ;;
+  headless-dryrun)
+    ./$AWS_NUKE_EXE --config $AWS_NUKE_CONFIG --force > log/aws-nuke-"$TIMESTAMP".log 2>&1
+  ;;
+  headless-nuke)
+    ./$AWS_NUKE_EXE --config $AWS_NUKE_CONFIG --force --no-dry-run > log/aws-nuke-"$TIMESTAMP".log 2>&1
+  ;;
+  setup)
+    curl -L $AWS_NUKE_URL > $AWS_NUKE_FILE.tar.gz 
+    tar xvzf $AWS_NUKE_FILE.tar.gz
+    mv $AWS_NUKE_FILE $AWS_NUKE_EXE 
+    chmod +x $AWS_NUKE_EXE
+    [ -d "log" ] || mkdir log
+    rm $AWS_NUKE_FILE.tar.gz
+  ;;
+  *)
+    echo "$1 is not a valid command"
+  ;;
+esac
